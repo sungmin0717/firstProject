8d3fb4154bf93736282f2c1f1d1780e9a28fad4c
DESKTOP-IEKEC4V\sungmin <sungmin48601@gmail.com>
2025-04-16 01:02:49 +0900

    4-16 서브 페이지 이메일 전송

--- src/main/java/admin/board/firstProject/subPage/service/SubPageServiceImpl.java
+++ src/main/java/admin/board/firstProject/subPage/service/SubPageServiceImpl.java
@@ -1,8 +1,17 @@
 package admin.board.firstProject.subPage.service;
 
+import java.util.concurrent.CompletableFuture;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import admin.board.firstProject.subPage.mapper.SubPageMapper;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,6 +20,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SubPageServiceImpl implements SubPageService {
 
+	private final SpringTemplateEngine templateEngine;
+	private final JavaMailSender mailSender;
 	private final SubPageMapper mapper;
 
 	// 이메일 중복확인
@@ -28,12 +39,49 @@ public class SubPageServiceImpl implements SubPageService {
 	
 	//DB INSERT 학생 정보 저장
 	@Override
-	public int information(String name, String email, String phon) {
+	@Async("asyncExecutor")
+	public CompletableFuture<Integer> information(String name, String email, String phon, String htmlName) {
 		
-		
-		
-		
-		return mapper.informationInsert(name,email,phon);
+	    try {
+	        MimeMessage mimeMessage = mailSender.createMimeMessage();
+	        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+
+	        String emailTitle = "emailPage";
+	        helper.setTo(email);
+	        helper.setSubject(name + "님 안녕하세요.");
+	        helper.setText(loadHtml(htmlName, emailTitle), true);
+	        mailSender.send(mimeMessage);
+
+	        int result = mapper.informationInsert(name, email, phon);
+	        return CompletableFuture.completedFuture(result);
+
+	    } catch (MessagingException e) {
+	        e.printStackTrace();
+	        return CompletableFuture.completedFuture(0);
+	    }
+			
 	}
+	
+	
+	
+//	템플릿엔진 html 요소를 얻어와 타임리프 처럼 사용 가능
+	public String loadHtml(String authKey, String htmlName) {
+
+		// org.tyhmeleaf.Context 선택!!
+		Context context = new Context();
+
+		// 타임리프가 적용된 HTML에서 사용할 값 추가
+		context.setVariable("authKey", authKey);
+
+		// templates/email 폴더에서 htmlName과 같은
+		// .html 파일 내용을 읽어와 String으로 변환
+		return templateEngine.process("email/" + htmlName, context);
+
+	}
+	
+	
+	
+
+
 
 }
