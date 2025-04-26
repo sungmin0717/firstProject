a7f9ad465d336aa03c49e2e4dc78893c8f3f8142
DESKTOP-IEKEC4V\sungmin <sungmin48601@gmail.com>
2025-04-26 20:11:22 +0900

    4-26 테스트 버전 완료

--- src/main/java/admin/board/firstProject/admin/controller/AdminContoller.java
+++ src/main/java/admin/board/firstProject/admin/controller/AdminContoller.java
@@ -1,14 +1,33 @@
 package admin.board.firstProject.admin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import admin.board.firstProject.admin.service.AdminService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping ("admin")
 public class AdminContoller {
 	private final AdminService service;
+	
+	
+	
+
+	@RequestMapping("")
+	public String adminLogin() {
+		
+		
+		
+		
+		return "admin/adminLogin";
+	}
+	
 	
 	
 
